f58e91144e071a6136afb91d7fe26cfe75d29b02
Halle <hallebristol2@gmail.com>
2023-10-13 13:02:12 -0400

    Denote Changes from the Official Solution

--- LibraryCatalogCard.cpp
+++ LibraryCatalogCard.cpp
@@ -1,16 +1,23 @@
+//Comments denote what differs from the solution in the course by Bill Weinman
+
+//#include <format>
 #include <iostream>
 
-struct CatalogCard {
-    const char *title {};
+//using std::format;
+//using std::cout;
+
+struct CatalogCard { //can also work in main()
+    const char *title {}; //could have also used character arrays
     const char *author {};
     const char *publisher {};
     const char *subject {};
-    long long ISBN {};
-    long long OCLC {};
-    double dewey {};
-    int published_year {};
-    int acquired_year {};
-    int quantity_in_stock {};
+    long long ISBN {}; //const char
+    long long OCLC {}; //const char
+    double dewey {}; //short int
+    //Second dewey variable to represent after the decimal for easier sorting
+    int published_year {}; //short int
+    int acquired_year {}; //short int
+    int quantity_in_stock {}; //short int 
 };
 
 int main() {
@@ -29,4 +36,7 @@ int main() {
 
     printf("Title: %s\nAuthor: %s\nPublisher: %s\nSubject: %s\nISBN: %lld\nOCLC: %lld\nDewey Decimal: %f\nPublished Year: %i\nAcquired Year: %i\nQuantity in Stock: %i",
     cc1.title, cc1.author, cc1.publisher, cc1.subject, cc1.ISBN, cc1.OCLC, cc1.dewey, cc1.published_year, cc1.acquired_year, cc1.quantity_in_stock);
+
+    //printed each individually
+    //cout << format{"Title: {}\n", cc1.title}; and etc.
 };
